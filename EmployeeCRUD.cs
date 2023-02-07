a7461c57dc195e94c78a354ad38305b410860850
VidyanandSawai17 <vidyanandsawai17@gmail.com>
2023-02-07 14:15:32 +0530

    Add project files.

---
+++ EmployeeCRUD.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+
+namespace Assignment22
+{
+    public class EmployeeCRUD
+    {
+        public List<Employee> employee;
+        internal int id;
+
+        public EmployeeCRUD()
+        {
+            employee = new List<Employee>()
+            {
+                new Employee { id = 1, name = "Emp1", salary = 23550 },
+                new Employee { id = 2, name = "Emp2", salary = 24550 },
+                new Employee { id = 3, name = "Emp3", salary = 25550 }
+
+            };
+        }
+
+        public void AddEmployee(Employee emp)
+        {
+
+            employee.Add(emp);
+
+        }
+
+        public void UpdateEmployee(Employee emp)
+        {
+            for (int i = 0; i < employee.Count; i++)
+            {
+                if (employee[i].id == emp.id)
+                {
+                    employee[i].name = emp.name;
+                    employee[i].salary = emp.salary;
+                    break; // to break the loop
+                }
+            }
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            for (int i = 0; i < employee.Count; i++)
+            {
+                if (employee[i].id == id)
+                {
+                    employee.Remove(employee[i]);
+                    break;
+                }
+            }
+        }
+
+
+        public List<Employee> List()
+        {
+            return employee;
+        }
+
+
+        public Employee ViewEmployee(int id)
+        {
+            Employee employee1 = new Employee();
+
+            for (int i = 0; i < employee.Count; i++)
+            {
+                if (employee[i].id == id)
+                {
+                    employee1 = employee[i];
+                    break;
+                }
+            }
+            return employee1;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment22
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        EmployeeCRUD employeeCRUD = new EmployeeCRUD();
+            int c = 0;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1. Empoyee List");
+                Console.WriteLine("2. Add Empoyee");
+                Console.WriteLine("3. Update Empoyee");
+                Console.WriteLine("4. Delete Empoyee");
+                Console.WriteLine("5. Display Product By Id");
+
+                Console.WriteLine("select above option");
+                int op = Convert.ToInt32(Console.ReadLine());
+                switch (op)
+                {
+                        case 1: 
+                        List<Employee> emp = employeeCRUD.List();
+                        Console.WriteLine("Id \t Name \t\tSalary");
+                        foreach (Employee item in emp)
+                        {
+                            Console.WriteLine($"{item.id} \t {item.name} \t\t{item.salary}");
+                        }
+                        break;
+
+                        case 2:
+                        Employee p1 = new Employee();
+                        Console.WriteLine("Enter Id");
+                        p1.id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter employee name");
+                        p1.name = Console.ReadLine();
+                        Console.WriteLine("Enter salary");
+                        p1.salary = Convert.ToInt32(Console.ReadLine());
+                        employeeCRUD.AddEmployee(p1);
+                        Console.WriteLine("Product added..");
+
+                        break;
+
+                        case 3:
+                        Employee p2 = new Employee();
+                        Console.WriteLine("Enter Id");
+                        p2.id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter new Employee name");
+                        p2.name = Console.ReadLine();
+                        Console.WriteLine("Enter new salary");
+                        p2.salary = Convert.ToInt32(Console.ReadLine());
+                        employeeCRUD.UpdateEmployee(p2);
+                        Console.WriteLine("Employee updated..");
+
+                        break;
+
+                        case 4:
+                        Console.WriteLine("Enter Id to delete");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        employeeCRUD.DeleteEmployee(id);
+                        Console.WriteLine("Product deleted..");
+
+                        break;
+
+
+                        case 5:
+                        Console.WriteLine("Enter product id to search");
+                        int id2 = Convert.ToInt32(Console.ReadLine());
+                        Employee employee = employeeCRUD.ViewEmployee(id2);
+                        if (employee.id > 0)
+                            Console.WriteLine($"Id = {employee.id} , Name ={employee.name}, Price= {employee.salary}");
+                        else
+                            Console.WriteLine("Product not found..");
+                        break;
+
+
+                        default:
+                        Console.WriteLine("Wrong option selection..");
+                        break;
+
+
+                }
+                Console.WriteLine("Select 1 for continue");
+                c = Convert.ToInt32((Console.ReadLine()));
+            }
+            while (c==1);
+
+
+        }
+    }
+}
